810ae49553b3581630535c5aaa4fa116201ba631
tembeaafricake <tembeaafricake@gmail.com>
2026-07-02 10:16:28 +0300

    added

--- next.config.js
+++ next.config.js
@@ -39,7 +39,10 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
